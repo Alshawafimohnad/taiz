a508740b7be6ab20e1020002804a034006f20f5b
Alshawafimohnad <alshawafimohnad@gmail.com>
2025-07-07 03:43:40 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const cacheName = 'almamlaka-v1';
+const filesToCache = [
+  '/',
+  '/index.html'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
